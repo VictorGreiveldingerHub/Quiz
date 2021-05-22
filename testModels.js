653dbdb0d94c7ff425d01eeb1215c2014d8df21a
Greiveldinger Victor <victorgreiveldinger@gmail.com>
2021-05-23 01:06:44 +0200

    Encore des tests

--- testModels.js
+++ testModels.js
@@ -1,42 +1,17 @@
-// Fichier de test pour voir si on récupère bien toutes les infos,
+require ('dotenv').config();
+
+// Trouver tous les users
 const User = require('./app/models/user');
-const newUser = new User({
-    id: 888,
-    firstname : "Victor",
-    lastname : "Greiveldinger",
-    email : "vicgrei@gmail.com"
+// User.findAll(console.log);
+
+// Trouver la question dont l'id est 3
+const Question = require('./app/models/question');
+// Question.findById(3, console.log);
+
+// Créer un level avec le nom "GodLike" et l'insérer en BDD
+const Level = require('./app/models/level');
+const newLevel = new Level({
+    name: "GodLike",
+    status: 1
 });
-
-console.log("Avant : ",newUser);
-
-// newUser.firstname = "Barb";
-// newUser.lastname = "Creus";
-
-// console.log("Après : ",newUser);
-
-// const CoreModel = require('./app/models/coreModels');
-// const newCoreModel = new CoreModel({});
-// newCoreModel.id = 1;
-// newCoreModel.status = 1;
-// console.log(newCoreModel);
-
-// // Petit test pour voir si User est bien une instance de CoreModel
-// console.log( newUser instanceof CoreModel);
-
-// // Pour tester le principe de "throw" une erreur dans le Setter
-// newUser.setId('Erreur'); // Pour avoir un message clair, et ARRETER le fonctionnement de toute l'application !
-// console.log("Bonjour ?"); // Ne sera jamais affiché, sauf si on la capture !
-
-// Donc on utilise try catch
-try {
-    // Tester l'erreur ...
-    // newUser.setId("Erreur");
-    
-    // Tester un bon déroulement
-    newUser.setId(35);
-    console.log("Instructions si il n'y a pas d'erreur... sinon on part dans le catch");
-} catch (error) {
-    console.log(error);
-};
-
-console.log("Bonjour ?"); // Sera maintenant affiché
+newLevel.insert(console.log);
